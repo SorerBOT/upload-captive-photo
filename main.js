933b5a415be15e21362260c49e48def21a0d5c32
SorerBOT <alonfiller@gmail.com>
2023-10-07 18:34:01 +0300

    gdrive test

---
+++ main.js
@@ -0,0 +1,103 @@
+// Needs cleanup and more modifications for your use.
+// Have token.json be an empty JSON ("{}"), we'll save the account creds there.
+// To create credentials.json, see https://rclone.org/drive
+const fs = require('fs');
+fsp = fs.promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/drive']; // Scope for everything
+const TOKEN_PATH = path.join(process.cwd(), 'token.json'); // Per user token
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json'); // Global keys (client ID / secret, etc)
+
+async function saveCredentials(client) {
+    const content = await fsp.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+      type: 'authorized_user',
+      client_id: key.client_id,
+      client_secret: key.client_secret,
+      refresh_token: client.credentials.refresh_token,
+    });
+    await fsp.writeFile(TOKEN_PATH, payload);
+}
+
+async function loadCredentials() {
+  try {
+    const content = await fsp.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+async function authorize() {
+  let client = await loadCredentials();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+async function listFiles(drive) {
+  const res = await drive.files.list({
+    pageSize: 10,
+    fields: 'nextPageToken, files(id, name)',
+  });
+  const files = res.data.files;
+  if (files.length === 0) {
+    console.log('No files found.');
+    return;
+  }
+
+  console.log('Files:');
+  files.map((file) => {
+    console.log(`${file.name} (${file.id})`);
+  });
+}
+
+async function uploadMultipart(drive, localPath, mimeType) {
+    const requestBody = {
+      name: path.basename(localPath),
+      fields: 'id',
+    };
+    if (requestBody) {
+        
+    }
+    const media = {
+      mimeType: mimeType,
+      body: fs.createReadStream(localPath),
+    };
+    try {
+      const file = await drive.files.create({
+        requestBody,
+        media: media,
+      });
+      console.log('File Id:', file.data.id);
+      return file.data.id;
+    } catch (err) {
+      // TODO(developer) - Handle error
+      throw err;
+    }
+}
+
+async function main()
+{
+    var client = await authorize();
+    var service = google.drive({version: 'v3', auth: client});
+    //await listFiles(service);
+    await uploadMultipart(service, "photo.jpg", "image/jpeg");
+}
+main()
