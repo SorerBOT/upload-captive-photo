cba3f7930bb15e42e1ffffb225e05ce15d01d31b
SorerBOT <alonfiller@gmail.com>
2023-10-07 23:58:24 +0300

    (Feat) a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UploadPhoto from "./Components/UploadPhoto/UploadPhoto";
-
+import "./App.css"
 function App() {
   return (
     <div className="App">
--- frontend/src/Components/UploadPhoto/UploadPhoto.js
+++ frontend/src/Components/UploadPhoto/UploadPhoto.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "../../Axios/axios";
+import "./index.css";
 
 function UploadPhoto() {
     const [uploadedFiles, setUploadedFiles] = useState([]); 
- 
+    const [fullname, setFullName] = useState("");
+    const [id, setId] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [found, setFound] = useState(false);
+
     const handleFileEvent = (e) => {
         const chosenFiles = Array.prototype.slice.call(e.target.files)
         handleUploadFiles(chosenFiles);
@@ -28,10 +33,34 @@ function UploadPhoto() {
         
         axios.post("/upload", data)
     } 
+    const handleSubmit = () => {
+    
+    }
     return <>
         <h2>העלה תמונה</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+              שם מלא:
+              <input type="text" value={fullname} onChange={(e) => setFullName(e.target.textContent)} />
+            </label>
+            <label>
+              מספר תעודת זהות:
+              <input type="text" value={id} onChange={(e) => setId(e.target.textContent)} />
+            </label>
+            <label>
+              מספר טלפון:
+              <input type="text" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.textContent)} />
+            </label>
+            <label>
+              נמצא:
+              <input type="checkbox" value={found} onChange={(e) => setFound(!found)} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
         <input id='fileUpload' type='file' multiple accept='image/*' onChange={handleFileEvent} />
+        
         <div>
+            
             {uploadedFiles.map((file, index) => (
                 <div key={index}>
                     {file.name}
